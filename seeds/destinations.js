8397a59ce59dcbcc174cb0523894b6a23f02bd25
Sean MacLennan <sgmaclennan@hotmail.com>
2020-07-22 15:21:48 -0600

    connect to db

---
+++ seeds/destinations.js
@@ -0,0 +1,85 @@
+const destinations = [
+  {
+    id:           1015,
+    title:        'Norway',
+    source:       'Lorem Picsum',
+    fileName:     'norway1015.jpg',
+    attribution:  {
+      source: 'Lorem Picsum',
+      credit: 'Alexey Topolyanskiy',
+      url:  ' https://picsum.photos/id/1015/300/300 '
+    }
+  },
+  {
+    id:           1018,
+    title:        'Japan',
+    fileName:     'japan1018.jpg',
+    attribution:  {
+      source: 'Lorem Picsum',
+      credit: 'Andrew Ridley',
+      url: 'https://picsum.photos/id/1018/300/300' 
+    }
+  },
+  {
+    id:           1016,
+    title:        'Australia',
+    fileName:     'australia1016',
+    attribution:  {
+      source: 'Lorem Picsum',
+      credit: 'Philippe Wuyts',
+      url:  'https://picsum.photos/id/1016/300/300'
+    }
+  },
+  {
+    id:           1043,
+    title:        'Canada',
+    fileName:     'canada1043.jpg',
+    attribution:  {
+      source: 'Lorem Picsum',
+      credit: 'Christain Joudrey',
+      url:  'https://picsum.photos/id/1043/300/300'
+    }
+  },
+  {
+    id:           1040,
+    title:        'Austria',
+    fileName:     'austria1040.jpg',
+    attribution:  {
+      source: 'Lorem Picsum',
+      credit: 'Rachel Davis',
+      url:  'https://picsum.photos/id/1040/300/300'
+    }
+  },
+  {
+    id:           1038,
+    title:        'Alaska',
+    fileName:     'alaska1038.jpg',
+    attribution:  {
+      source: 'Lorem Picsum',
+      credit: 'Marcello Quinan',
+      url:  'https://picsum.photos/id/1038/300/3004'
+    }
+  },
+  {
+    id:           100,
+    title:        'Wolf',
+    fileName:     'israel100.jpg',
+    attribution:  {
+      source: 'Lorem Picsum',
+      credit: 'Tini Rataj',
+      url:  'https://picsum.photos/id/100/300/300'
+    }
+  },
+  {
+    id:           101,
+    title:        'North Korea',
+    fileName:     'north-korea101.jpg',
+    attribution:  {
+      source: 'Lorem Picsum',
+      credit: 'Christian Bordenhorst',
+      url:  'https://picsum.photos/id/101/300/300'
+    }
+  },
+];
+
+module.exports = destinations;
---
+++ views/README.md
@@ -0,0 +1,12 @@
+CPRG 210 Assignment #3:
+July 19, 2020
+Mock-up travel agency 3 page website with gallery.
+By Sean MacLennan
+Fonts "Kaushan script" and "Montserrat" by Google Fonts
+Form templates by W3 Schools
+Code sampled for some nav propeties by Tony Grimes
+All images by Lorem Picsum
+Some flex box code from CSS Tricks
+My apologies for the late submission
+
+Tony, I have added a gallery with links to my index.html home page. However I was not able to make the images with their labels and a href links into an array sucessfully. So what I have done to at least demonstrate that I have some understanding of loops is make the gallery page a for-each loop of images. I apologize for not being able to successfuly meet the exact requirements of combining these two concepts. 
